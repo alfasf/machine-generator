e543812941d992648fc81600a12e482a0a8beb15
alfasf <diazalfredo83@gmail.com>
2017-12-03 21:52:35 -0800

    getting api for wikiquote

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 $(document).ready(function(){
   $("#newQuote").on("click", function(e){
   	e.preventDefault();
-    $.ajax({
-    	type: 'GET',
-    	url: "https://cors.io/?http://quotesondesign.com/api/3.0/api-3.0.json?callback=my_function",
-    	});
+  	var wikiurl = "https://en.wikiquote.org/w/api.php?format=json";
+    $.ajax( {
+	    url: wikiurl,
+	    data: queryData,
+	    dataType: 'json',
+	    type: 'GET',
+	    headers: { 'Api-User-Agent': 'Example/1.0' },
+	    success: function(data) {
+	       // do something with data
+	       $('blockquote').html(data);
+	    }
+	});
 
-    });
+    }); //end of on click 
 
 }); 
